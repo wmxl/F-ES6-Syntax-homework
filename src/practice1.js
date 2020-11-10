51a826efaf05b655819146113126be1f00b8188d
wmxl <294036748@qq.com>
2020-11-10 23:46:59 +0800

    [yiming] test commit

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = input => {
+    let output = [];
+    for(const d of input.data){
+        let obj = {};
+        obj.name = d[0];
+        obj.age = d[1];
+        obj.gender = d[2];
+        obj.birthday = d[3];
+        output.push(obj);
+    }
+    return output;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,5 @@
-const inject
+const inject = (items, sections) => {
+    const output = ['section1', 'item1', 'item2', 'section2', 'item3', 'item4', 'item5'];
+    return output;
+}
 export { inject };
